e4974086f82fc478d7779d4d58941bb0b3e04cb3
unknown <mithun.k@gradious.com>
2025-10-11 10:10:37 +0530

    code

---
+++ friday10-10-2025/script.js
@@ -0,0 +1,54 @@
+let ele = document.querySelector(".ele");
+// let body = document.querySelector("body");
+// let body = ele.parentElement;
+// body.style.backgroundColor = "red";
+
+// console.log(ele.parentElement);
+
+
+let eleChilds = ele.childNodes;
+console.log(eleChilds[0].nextSibling);
+
+
+
+// ele.childNodes[1].firstChild.nodeValue = "Asdasd";
+
+// console.log(ele.parentNode.style.backgroundColor="red");
+// console.log(ele.nextSibling);
+// console.log(innerHeight, innerWidth);
+// console.log(outerHeight, outerWidth);
+
+// console.log(window.location.href); // Current URL
+// console.log(window.location.hostname); // Domain name
+// console.log(window.location.pathname); // Path after domain
+
+// setTimeout(() => {
+// 	window.location.reload();
+// }, 1000);
+
+// console.log("Browser Name:", window.navigator.appName);
+// console.log("Browser Version:", window.navigator.appVersion);
+// console.log("Platform:", window.navigator.platform);
+// console.log("User Agent:", window.navigator.userAgent);
+
+// console.log((window.document.title = "sad")); // Page title
+// console.log((window.document.URL)); // Full page URL
+// console.log(window.document.body.childNodes); // Body element
+
+// console.log("Screen Width:", window.screen.width);
+// console.log("Screen Height:", window.screen.height);
+// console.log("Available Width:", window.screen.availWidth);
+// console.log("Available Height:", window.screen.availHeight);
+
+// console.log(window.history.back()); // Number of pages in session history
+
+// Navigate back or forward
+// window.history.back();
+// window.history.forward();
+
+// console.log("Screen Width:", screen.width);
+// console.log("Screen Height:", screen.height);
+// console.log("Available Width:", screen.availWidth);
+// console.log("Available Height:", screen.availHeight);
+// console.log("Color Depth:", screen.colorDepth);
+// console.log("Pixel Depth:", screen.pixelDepth);
