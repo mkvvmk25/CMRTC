c6c0155b00164f35e9a343e2c0396409b8177c84
unknown <mithun.k@gradious.com>
2025-10-25 14:10:51 +0530

    code

--- java_dsa/DAC.java
+++ java_dsa/DAC.java
@@ -23,9 +23,48 @@ public class DAC {
 
   }
 
+  static int heatersSolve(int houses[], int heaters[])
+  {
+    int minRadius = 0;
+    Arrays.sort(heaters);
+    for (int house : houses) {
+      int nearHeater = bshh(heaters, house);
+      if(minRadius < nearHeater)
+      {
+        minRadius = nearHeater;
+      }
+
+    }
+    return minRadius;
+  }
+
+  static int  bshh(int heaters[], int house)
+  {
+    int minDiff = Integer.MAX_VALUE;
+    int n = heaters.length;
+    int i = 0, j = n - 1;
+    while (i <= j) {
+      int m = (i + j) / 2;
+      int diff = Math.abs(house - heaters[m]);
+      if (diff < minDiff) {
+        minDiff = diff;
+      }
+
+      if (house < heaters[m]) {
+        j = m - 1;
+      } else if (house > heaters[m]) {
+        i = m + 1;
+      }
+    }
+    return minDiff;
+  }
+
   public static void main(String[] args) {
-    int arr[] = { 2, 5, 1, 0, 4, 23, 10 };
-    Arrays.sort(arr);
-    int ind = bs(arr,50);
+    //                  *  *     *  
+    int houses[] = { 2, 5, 1, 3, 4, 9, 10 };
+    int heaters[] = { 1, 4, 5 }; // sort 
+    //               ijm
+    
+    heatersSolve(houses, heaters);
   }
 }
